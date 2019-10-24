c39bf03481b4b6820ea211e9f315e74d56cd99e8
GBTC440015UR <GBTC440015UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-10-23 21:03:31 -0400

    SpringBoot_403 USING ROLES FOR PAGE

--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.naming.AuthenticationException;
 
@@ -22,17 +23,27 @@ public class SecurityConfiguration extends
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests().anyRequest().authenticated()
-                .and().formLogin().loginPage("/login").permitAll();
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        http.authorizeRequests()
+                .antMatchers("/")
+                .access("hasAnyAuthority('USER','ADMIN')")
+                        .antMatchers("/admin")
+                        .access("hasAuthority('ADMIN')")
+                                .anyRequest().authenticated()
+                                .and()
+                                .formLogin().loginPage("/login").permitAll()
+                                .and()
+                                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                                        .logoutSuccessUrl("/login").permitAll();
     }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth)
-            throws Exception {
-        auth.inMemoryAuthentication().withUser("user")
-                .password(passwordEncoder().encode("password")).authorities("USER");
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+        .withUser("dave").password(passwordEncoder().encode("begreat"))
+        .authorities("ADMIN")
+                .and().withUser("user").password(passwordEncoder().encode("password")).authorities("USER");
     }
 
 
